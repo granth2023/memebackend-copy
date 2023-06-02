72b2e330f36272b79d61351af956da4417e5e033
granth2023 <grantharris38@gmail.com>
2023-06-02 13:45:46 -0400

    updated deploy

--- api/DB/newfirstseed.js
+++ api/DB/newfirstseed.js
@@ -1,7 +1,3 @@
-
-
-
-
 import axios from 'axios';
 // import { writeFile } from 'fs/promises';
 import path from 'path';
--- api/index.js
+++ api/index.js
@@ -2,25 +2,23 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import routes from './Routes/firstRoutes.js'
+// Database and data
+import db from './DB/connection.js'
+
+// import {submissionsData} from '../mememaker_backend/DB/newfirstseed.js'
+// import {createMemesJSON} from './DB/newfirstseed.js'
+
+
 const app = express()
 const port = 2000
 //comment 
 //2nd comment
- 
-  
 
-  
-
-import {createMemesJSON} from './DB/newfirstseed.js'
-// import {submissionsData} from '../mememaker_backend/DB/newfirstseed.js'
-
-// Database and data
-import db from './DB/connection.js'
 //import datas from '../mememaker_backend/Data/datas.json' assert {type: 'json'}
 
 
-db.on('connected', async () => {
-  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+// db.on('connected', async () => {
+//   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 // createMemesJSON()
 // .then((response) => {
 //   console.log('Memes created successfully:', response);
@@ -30,7 +28,7 @@ db.on('connected', async () => {
   //         console.log('Submissions created successfully: ', response)
   //       })
     
-})
+// })
 
 //why does the folder come up as tests and datas? also why on compass. 
 //need routes, controllers, second model, then i need all front end. 
@@ -40,9 +38,9 @@ app.use(express.json());
 // Routes
 app.use('/', routes);
 
-// app.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`);
-//   });
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
 
 
-export default app 
+// export default app 
