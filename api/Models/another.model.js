cf1acdc316478861815a6e8eb0539495918ba4ff
granth2023 <grantharris38@gmail.com>
2023-06-03 17:48:00 -0400

    toptextstyles

--- api/Models/another.model.js
+++ api/Models/another.model.js
@@ -1,18 +1,19 @@
 import mongoose from 'mongoose'
 
 const submissionSchema = new mongoose.Schema({
-    Meme: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Meme',
-        required: false
-      },
-    dataCreated: String,
-    bottomText: String,
-    topText: String,
-    topTextPosition: String,
-    bottomTextPosition: String
-  })
-
+  Meme: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Meme',
+    required: false
+  },
+  dataCreated: String,
+  bottomText: String,
+  topText: String,
+  topTextPosition: String,
+  bottomTextPosition: String,
+  topTextStyles: String,
+  bottomTextStyles: String
+});
 const Submission = mongoose.model('Submission', submissionSchema);
 
 export default Submission; 
